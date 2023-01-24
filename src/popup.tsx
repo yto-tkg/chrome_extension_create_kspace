0dd11332eb4f1fa33b2f56a3bdb7176cd9421275
yto-tkg <yuto-takagi@cybozu.co.jp>
2023-01-25 08:27:12 +0900

    fix app form unused

--- src/popup.tsx
+++ src/popup.tsx
@@ -3,23 +3,11 @@ import ReactDOM from "react-dom";
 import AppForm from "./postAppForm";
 import SpaceForm from "./postSpaceForm";
 
-type FormData = {
-  host: string
-  userName: string
-  spaceName: string
-  isIncliment: boolean
-  count: number
-  isMultiThread: boolean
-  isPrivate: boolean
-  isGuest: boolean
-}
-
 const Popup = () => {
   return (
     <>
       <div style={{ minWidth: "300px" }}>
         <SpaceForm />
-        //<AppForm />
       </div>
     </>
   );
